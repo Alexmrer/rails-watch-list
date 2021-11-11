dc76746f6d6b8cb1133669f3d46f4a88a3609bb1
Alex Meurer <alxmrer@gmail.com>
2021-11-11 14:54:42 +0100

    Add Movies API

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+require 'open-uri'
+require 'json'
 # This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
@@ -5,3 +7,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Movie.destroy_all
+
+url = 'http://tmdb.lewagon.com/movie/top_rated'
+movies = open(url).read
+
+result = JSON.parse(movies)['results']
+result = result.sample(20)
+
+result.each do |movie|
+  Movie.create!(title: movie['title'],
+                overview: movie['overview'],
+                poster_url: "https://image.tmdb.org/t/p/w500/#{movie['poster_path']}",
+                rating: movie['vote_average'])
+end
